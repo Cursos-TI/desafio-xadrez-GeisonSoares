a1f9e791a32e0fcadddc4cafbc9f8b7cbe7ef820
GeisonSoares <geison_bastos@hotmail.com>
2025-05-13 03:59:58 +0000

    Acrescimo do Cavalo movimentação em L

--- xadrez.c
+++ xadrez.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     // Movimentação da Torre (5 casas para a direita) usando FOR
     int i;
     printf("Movimento da Torre:\n");
-    for (i = 1; i <= 5; i++) {
+    for (i = 1; i <= 5; i++) 
+    {
         printf("Direita\n");
     }
 
     // Movimentação do Bispo (5 casas na diagonal superior direita) usando WHILE
     int j = 1;
     printf("\nMovimento do Bispo:\n");
-    while (j <= 5) {
+    while (j <= 5) 
+    {
         printf("Cima, Direita\n");
         j++;
     }
@@ -19,10 +22,25 @@ int main() {
     // Movimentação da Rainha (8 casas para a esquerda) usando DO-WHILE
     int k = 1;
     printf("\nMovimento da Rainha:\n");
-    do {
+    do 
+    {
         printf("Esquerda\n");
         k++;
     } while (k <= 8);
 
+    // Movimentação do Cavalo (2 casas para baixo, 1 para a esquerda) usando loops aninhados for e while
+    printf("\nMovimento do Cavalo:\n");
+
+    for (int m = 1; m <= 2; m++) 
+    { 
+        printf("Baixo\n");
+
+        int n = 0;
+        while (n < 1 && m == 2) 
+        { 
+            printf("Esquerda\n");
+            n++;
+        }
+    }
     return 0;
 }
