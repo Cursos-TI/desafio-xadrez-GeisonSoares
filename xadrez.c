f2c1c3992a1bd471dc197b6decb4a53e0256d94b
GeisonSoares <geison_bastos@hotmail.com>
2025-05-13 01:34:52 +0000

    alteração de printf para se adequar ao que a atividade pede.

--- xadrez.c
+++ xadrez.c
@@ -12,7 +12,7 @@ int main() {
     int j = 1;
     printf("\nMovimento do Bispo:\n");
     while (j <= 5) {
-        printf("Cima Direita\n");
+        printf("Cima, Direita\n");
         j++;
     }
 
